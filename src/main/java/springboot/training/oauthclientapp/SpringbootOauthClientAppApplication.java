c8c237c69c5897214efc0ca3e2acc85babdb46e2
cnoriega20 <c79.noriega@gmail.com>
2020-08-22 17:31:13 -0500

    Add project structure and gitignore modifications

---
+++ src/main/java/springboot/training/oauthclientapp/SpringbootOauthClientAppApplication.java
@@ -0,0 +1,13 @@
+package springboot.training.oauthclientapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootOauthClientAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootOauthClientAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/springboot/training/oauthclientapp/SpringbootOauthClientAppApplicationTests.java
@@ -0,0 +1,13 @@
+package springboot.training.oauthclientapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootOauthClientAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
